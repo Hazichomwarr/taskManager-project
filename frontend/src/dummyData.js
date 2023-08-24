73cbece3cd8048c45775e29ff1172abbafe0dc12
Hamza Mare <batchiy6@gmail.com>
2023-08-24 01:43:21 -0400

    final commit

---
+++ frontend/src/dummyData.js
@@ -0,0 +1,27 @@
+const tasks = [
+  {
+    id: 2,
+    title: "customers call",
+    description: "call rebelious customers",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "inventory",
+    description: "inventory day with insvestors",
+    completed: true,
+  },
+  {
+    id: 4,
+    title: "Weekly reports",
+    description: "Sending the weekly reports for overdue invoices",
+    completed: false,
+  },
+  {
+    id: 5,
+    title: "Order Release",
+    description:
+      "Check out customers accounts and release or block accordingly",
+    completed: true,
+  },
+];
